9f204d4b0ea67e4899db9e038d80e5d28685fe54
mirandaisa18 <misfamilia.yanina35@gmail.com>
2026-02-19 11:03:53 -0400

    Agrego README.md al repositorio water

---
+++ water/README.md/# Proyecto Git - 5to A.rb
@@ -0,0 +1,23 @@
+# Proyecto Git - 5to A
+
+## Descripción
+Este es un proyecto para la evaluación práctica de Git y GitHub.
+
+## Integrantes del equipo
+echo "- santos lopez" >> README.md
+echo "- carlos perez " >> README.md
+echo "- Jesus Matinez " >> README.md
+echo "- Yanina Miranda" >> README.md
+echo "- exner sibrian " >> README.md
+ 
+
+## Fecha
+Febrero 2026
+
+## Comandos Git utilizados
+- git init
+- git add
+- git commit
+- git branch
+- git merge
+- etc.
